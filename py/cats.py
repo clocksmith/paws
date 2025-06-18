6cca3ac9a801ce0c212a82a07a49d02e121a0226
ant <591699+clocksmith@users.noreply.github.com>
2025-06-18 12:46:51 -0400

    Update cats.py to have h5 as default persona

--- py/cats.py
+++ py/cats.py
@@ -255,7 +255,7 @@ def main_cli():
     parser.add_argument(
         "-p",
         "--persona",
-        default="personas/sys_h3.md",
+        default="personas/sys_h5.md",
         help="Path to a persona file to prepend to the entire output.",
     )
     parser.add_argument(
